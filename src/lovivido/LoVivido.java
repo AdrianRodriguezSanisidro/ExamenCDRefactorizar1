7fd7f72b3ba0519c8c9acf4f9f8d21e74116390c
Adry <Adry@Sanisidro>
2016-12-12 09:08:02 +0100

    Crear metodo calcularEdad donde meto las varibles y calculos.En la main llamo a este metodo.

--- src/lovivido/LoVivido.java
+++ src/lovivido/LoVivido.java
@@ -9,7 +9,12 @@ import javax.swing.JOptionPane;
 public class LoVivido {
 
     public static void main(String[] args) {
-        String nombre;
+        calcularEdad();
+        System.exit(0);
+
+    }
+public static void calcularEdad(){
+    String nombre;
         String edad;
 
         int meses;
@@ -31,9 +36,5 @@ public class LoVivido {
         JOptionPane.showMessageDialog(null, "Días: " + dias, "Numero de días vividos de " + nombre, JOptionPane.INFORMATION_MESSAGE);
 
         JOptionPane.showMessageDialog(null, "Horas: " + horas, "Numero de horas vividos de " + nombre, JOptionPane.INFORMATION_MESSAGE);
-
-        System.exit(0);
-
-    }
-
+}
 }
